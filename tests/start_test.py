15be60ce3cba7d5e7a23d84912d17b96fd22a5cd
tanner666 <tjg5@njit.edu>
2024-04-13 21:45:18 -0400

    fixed routing code

--- tests/start_test.py
+++ tests/start_test.py
@@ -55,3 +55,4 @@ async def test_create_and_delete_qr_code():
             qr_filename = qr_code_url.split('/')[-1]
             delete_response = await ac.delete(f"/qr-codes/{qr_filename}", headers=headers)
             assert delete_response.status_code == 204  # No Content, successfully deleted
+            
